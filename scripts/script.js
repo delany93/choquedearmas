544c9d6c27cdea2675ed0c7839101e8237101967
Delany Lopez <delanyjoeliz@gmail.com>
2023-05-21 17:01:38 -0300

    hola

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,22 @@
+// Pagina principal // 
+let personajeUsuario = document.querySelector('.personaje-usuario');
+let armaUsuario = document.querySelector('.arma-usuario')
+let perfilesJugadores = document.querySelector('.perfiles-jugadores');
+let htmlBatallaChat = document.querySelector('.batalla-chat');
+let htmlResultado = document.querySelector('.resultado')
+
+
+armaUsuario.classList.add('inactive');
+perfilesJugadores.classList.add('inactive');
+htmlBatallaChat.classList.add('inactive');
+htmlResultado.classList.add('inactive');
+
+
+
 let usuario 
 let pcENEMY = Math.floor(Math.random() * 3) + 1;
 
-let personajeUsuario = document.querySelector('.personaje-usuario')
-let armaUsuario = document.querySelector('.arma-usuario');
-armaUsuario.classList.add('inactive');
+
 
 
 // Frase pagina principal // 
@@ -14,12 +27,12 @@ let frases = [
     "La guerra espera a los valientes, selecciona tu equipo y únete a la batalla.", 
     "Antes de enfrentar al enemigo, elige tus herramientas con sabiduría y forja tu camino hacia la victoria."
 ]
-let htmlBatalla = document.querySelector('.batalla');
+
 let htmlFraseAleatoria = document.createElement('p');
 let fraseAleatoria = obtenerFraseAleatoria();
 
 htmlFraseAleatoria.innerHTML = fraseAleatoria;
-htmlBatalla.appendChild(htmlFraseAleatoria);
+htmlBatallaChat.appendChild(htmlFraseAleatoria);
 
 function obtenerFraseAleatoria() {
     var indiceAleatorio = Math.floor(Math.random() * frases.length);
@@ -28,14 +41,16 @@ function obtenerFraseAleatoria() {
 
 // 
 let guardianDeLaRoca = document.getElementById('roca');
-guardianDeLaRoca.addEventListener('click', elegirArma);
+guardianDeLaRoca.addEventListener('click', cambiarImagen);
 let maestroDeLasHojas = document.getElementById('papel');
 maestroDeLasHojas.addEventListener('click', elegirArma)
 let reinaDeLaCorte = document.getElementById('tijera');
 reinaDeLaCorte.addEventListener('click', elegirArma);
 
-function elegirArma() {
-    armaUsuario.classList.remove('inactive');
-    personajeUsuario.classList.add('inactive');
-}
+  
+
+// function elegirArma() {
+//     armaUsuario.classList.remove('inactive');
+//     personajeUsuario.classList.add('inactive');
+// }
 
