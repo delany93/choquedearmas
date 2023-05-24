62d63c9b19af2760e001a5568b8e4d22046cf3e1
Delany Lopez <delanyjoeliz@gmail.com>
2023-05-23 21:36:59 -0300

    html y css done

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,22 @@
 // Pagina principal // 
 let personajeUsuario = document.querySelector('.personaje-usuario');
-let armaUsuario = document.querySelector('.arma-usuario')
+let htmlSegundoMenu = document.querySelector('.segundo-menu')
 let perfilesJugadores = document.querySelector('.perfiles-jugadores');
 let htmlBatallaChat = document.querySelector('.batalla-chat');
 let htmlResultado = document.querySelector('.resultado')
 
+let botonSeleccionar = document.querySelector('.start-btn');
 
-armaUsuario.classList.add('inactive');
-perfilesJugadores.classList.add('inactive');
-htmlBatallaChat.classList.add('inactive');
+// Inicio
+htmlSegundoMenu.classList.add('inactive');
+
+// htmlBatallaChat.classList.add('inactive');
 htmlResultado.classList.add('inactive');
 
 
 
-let usuario 
+
+let usuario
 let pcENEMY = Math.floor(Math.random() * 3) + 1;
 
 
@@ -24,7 +27,7 @@ let frases = [
     "Prepárate para la batalla, el desafío está por comenzar.",
     "El destino te llama a la acción, selecciona tu equipo y adéntrate en la batalla.",
     "Elige sabiamente tus armas y prepárate para la épica contienda que se avecina.",
-    "La guerra espera a los valientes, selecciona tu equipo y únete a la batalla.", 
+    "La guerra espera a los valientes, selecciona tu equipo y únete a la batalla.",
     "Antes de enfrentar al enemigo, elige tus herramientas con sabiduría y forja tu camino hacia la victoria."
 ]
 
@@ -37,20 +40,25 @@ htmlBatallaChat.appendChild(htmlFraseAleatoria);
 function obtenerFraseAleatoria() {
     var indiceAleatorio = Math.floor(Math.random() * frases.length);
     return frases[indiceAleatorio];
-    }
+}
 
 // 
-let guardianDeLaRoca = document.getElementById('roca');
-guardianDeLaRoca.addEventListener('click', cambiarImagen);
-let maestroDeLasHojas = document.getElementById('papel');
-maestroDeLasHojas.addEventListener('click', elegirArma)
-let reinaDeLaCorte = document.getElementById('tijera');
-reinaDeLaCorte.addEventListener('click', elegirArma);
+let guardianDeLaRoca = document.getElementById('guardian input');
+let maestroDeLasHojas = document.getElementById('maestro input');
+let reinaDeLaCorte = document.getElementById('reina input');
 
-  
 
-// function elegirArma() {
-//     armaUsuario.classList.remove('inactive');
-//     personajeUsuario.classList.add('inactive');
-// }
+botonSeleccionar.addEventListener('click', segundoMenu);
+
+function segundoMenu() {
+    htmlSegundoMenu.classList.remove('inactive');
+    personajeUsuario.classList.add('inactive');
+    htmlBatallaChat.classList.remove('inactive');
+    
+
+}
+function elegirArma() {
+    armaUsuario.classList.remove('inactive');
+    personajeUsuario.classList.add('inactive');
+}
 
