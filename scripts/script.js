8269622cf7262f896445eb3399457df6702d7e36
Delany Lopez <delanyjoeliz@gmail.com>
2023-06-05 16:20:53 -0300

    aun sin solucionar el error

--- scripts/script.js
+++ scripts/script.js
@@ -266,7 +266,7 @@ function fraseAleatoriaGanadora() {
 
         `La valentía de <b>${nombreJugador}</b> se hace evidente cuando utiliza con maestría la <b>${jugadorArma}</b> contra el <b>Esqueleto Infernal</b>. El monstruo queda debilitado ante el poderoso ataque y ${nombreJugador} emerge como el vencedor con esta victoria. <b>GANASTE</b>`,
 
-        `Con un movimiento certero, <b>${nombreJugador}</b> utiliza su <b>${jugadorArma}</b> para superar al <b>Esqueleto Infernal</b>. El monstruo se desmorona ante el impacto, mientras ${nombreJugador}se erige como una verdadera fuerza en esta encarnizada batalla, obteniendo una gloriosa victoria. <b>GANASTE</b>`,
+        `Con un movimiento certero, <b>${nombreJugador}</b> utiliza su <b>${jugadorArma}</b> para superar al <b>Esqueleto Infernal</b>. El monstruo se desmorona ante el impacto, mientras ${nombreJugador}se erige como una verdadera fuerza en esta encarnizada batalla. <b>GANASTE</b>`,
     ]
     let fraseAleatoriaGanadora = obtenerFraseAleatoria(frasesGanadoras);
     return fraseAleatoriaGanadora
@@ -277,13 +277,13 @@ function fraseAleatoriaPerdedora() {
     let frasesPerdedoras = [
         `<b>${nombreJugador}</b> desata el poder de la <b>${jugadorArma}</b> contra el temible <b>Esqueleto Infernal</b>. Sin embargo, el Esqueleto Infernal responde con un feroz ataque de su <b>${computadoraArma}</b>. Lamentablemente, ${nombreJugador} sufre una derrota en esta épica batalla. <b>PIERDES</b>`,
 
-        `<b>${nombreJugador}</b> desata la fuerza implacable de la <b>${jugadorArma}</b> contra el malévolo <b>Esqueleto Infernal</b>. Sin embargo, el Esqueleto Infernal se defiende hábilmente con su <b>${computadoraArma}</b>. ${nombreJugador} lucha valientemente, pero finalmente sufre una derrota en esta encarnizada batalla. <b>PIERDES</b>`,
+        `<b>${nombreJugador}</b> desata la fuerza de la <b>${jugadorArma}</b> contra el malévolo <b>Esqueleto Infernal</b>. Sin embargo, el Esqueleto Infernal se defiende hábilmente con su <b>${computadoraArma}</b>. ${nombreJugador} lucha valientemente, pero finalmente sufre una derrota. <b>PIERDES</b>`,
 
         `<b>${nombreJugador}</b> invoca el poderío de la <b>${jugadorArma}</b> contra el aterrador <b>Esqueleto Infernal</b>. Pero el Esqueleto Infernal contraataca con su feroz <b>${computadoraArma}</b>. A pesar de la valentía de ${nombreJugador}, esta vez debe aceptar la derrota en esta épica confrontación. <b>PIERDES</b>`,
 
-        `<b>${nombreJugador}</b> empuña la <b>${jugadorArma}</b> con destreza frente al imponente <b>Esqueleto Infernal</b>. Sin embargo, el Esqueleto Infernal contraataca con su letal <b>${computadoraArma}</b>. A pesar de la feroz lucha de ${nombreJugador}, finalmente no logra prevalecer y sufre una derrota en esta batalla intensa. <b>PIERDES</b>`,
+        `<b>${nombreJugador}</b> empuña la <b>${jugadorArma}</b> con destreza frente al imponente <b>Esqueleto Infernal</b>. Sin embargo, el Esqueleto Infernal contraataca con su letal <b>${computadoraArma}</b>. A pesar de la feroz lucha de ${nombreJugador} sufre una derrota en esta batalla intensa. <b>PIERDES</b>`,
 
-        `<b>${nombreJugador}</b> libera su ataque más poderoso con la <b>${jugadorArma}</b>. Ambos combatientes desatan sus mejores habilidades, pero el <b>Esqueleto Infernal</b> se muestra implacable con su letal <b>${computadoraArma}</b>. A pesar de la valentía de ${nombreJugador}, su derrota se hace inevitable en esta encarnizada contienda. <b>PIERDES</b>`,
+        `<b>${nombreJugador}</b> libera su ataque más poderoso con la <b>${jugadorArma}</b>. Ambos combatientes desatan sus mejores habilidades, pero el <b>Esqueleto Infernal</b> se muestra implacable con su letal <b>${computadoraArma}</b>. A pesar de la valentía de ${nombreJugador}, su derrota se hace inevitable. <b>PIERDES</b>`,
 
     ];
 
@@ -294,13 +294,13 @@ function fraseAleatoriaPerdedora() {
 function fraseAleatoriaEmpate() {
     botonAtacarHabilitar()
     let frasesEmpate = [
-        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> desatan el poderío de la <b>${jugadorArma}</b> en un enfrentamiento épico. Sus ataques chocan con una fuerza arrolladora, pero ninguno de ellos logra prevalecer. La batalla termina en un empate, dejando a ambos luchadores exhaustos pero determinados a continuar la lucha. <b>EMPATE</b>.`,
+        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> desatan el poderío de la <b>${jugadorArma}</b>. Sus ataques chocan con una fuerza arrolladora, pero ninguno de ellos logra prevalecer. La batalla termina en un empate, dejando a ambos luchadores exhaustos pero determinados a continuar la lucha. <b>EMPATE</b>.`,
 
-        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> se enfrentan usando la <b>${jugadorArma}</b> como arma principal. Sus movimientos son rápidos y precisos, pero ninguno de ellos logra obtener la ventaja sobre el otro. La batalla termina en un empate, dejando a ambos con la determinación de superarse en el siguiente encuentro. <b>EMPATE</b>.`,
+        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> se enfrentan usando la <b>${jugadorArma}</b>. Sus movimientos son rápidos y precisos, pero ninguno de ellos logra obtener la ventaja sobre el otro. La batalla termina en un empate, dejando a ambos con la determinación de superarse en el siguiente encuentro. <b>EMPATE</b>.`,
 
-        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> se enfrentan con la <b>${jugadorArma}</b>, demostrando su destreza en el combate. Los filos chocan en un duelo impresionante, pero ninguno logra ganar la batalla. El enfrentamiento termina en un empate, dejando a ambos guerreros con la sensación de que aún hay mucho por demostrar. <b>EMPATE</b>.`,
+        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> se enfrentan con la <b>${jugadorArma}</b>. Los filos chocan en un duelo impresionante, pero ninguno logra ganar la batalla. El enfrentamiento termina en un empate, dejando a ambos guerreros con la sensación de que aún hay mucho por demostrar. <b>EMPATE</b>.`,
 
-        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> libran un duelo impresionante con la <b>${jugadorArma}</b> como su arma principal. Sus movimientos son ágiles y precisos, entrelazando sus filos en una danza de habilidad y destreza. Sin embargo, ninguno de ellos logra alcanzar la victoria, y el enfrentamiento termina en un empate. Ambos guerreros se retiran con la determinación de superarse en la siguiente ronda. <b>EMPATE</b>.`,
+        `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> libran un duelo impresionante con la <b>${jugadorArma}</b>. Sus movimientos son ágiles y precisos, entrelazando sus filos en una danza de habilidad y destreza. Sin embargo, ninguno de ellos logra alcanzar la victoria, y el enfrentamiento termina en un empate. <b>EMPATE</b>.`,
 
         `<b>${nombreJugador}</b> y el <b>Esqueleto Infernal</b> se enfrentan con sus letales <b>${jugadorArma}</b> en una batalla llena de velocidad y agilidad. Sus movimientos son rápidos y precisos, sin embargo, ninguno logra obtener la ventaja sobre el otro. La batalla culmina en un empate, dejando a ambos guerreros con la sensación de que aún hay mucho por demostrar en futuros encuentros. <b>EMPATE</b>.`
 
